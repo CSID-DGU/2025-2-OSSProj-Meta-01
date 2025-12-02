7aea8fd686f4ca53093c6f484409d8418aa5ec77
ysgyeong00 <ysgyeong00@dgu.ac.kr>
2025-12-02 17:51:52 +0900

    fix: url 이름 설정

--- backend/src/scholarship/urls.py
+++ backend/src/scholarship/urls.py
@@ -7,8 +7,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", ScholarshipListView.as_view()),
-    path("recommendations/", RecommendationListView.as_view()),
-    path("<int:scholarship_id>/", ScholarshipDetailView.as_view()),
-    path("<int:scholarship_id>/bookmark/", ScholarshipBookmarkToggleView.as_view()),
+    path("", ScholarshipListView.as_view(), name='scholarship_list'),
+    path("recommendations/", RecommendationListView.as_view(), name='my_recommendations'),
+    path("<int:scholarship_id>/", ScholarshipDetailView.as_view(), name='scholarship_detail'),
+    path("<int:scholarship_id>/bookmark/", ScholarshipBookmarkToggleView.as_view(), name='scholarship_bookmark_toggle'),
 ]
