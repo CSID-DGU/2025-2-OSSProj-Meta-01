ab40d6034fbe8108503b5be74ce090c762d86e5d
ysgyeong00 <ysgyeong00@dgu.ac.kr>
2025-12-06 04:43:15 +0900

    fix: doc_id 데이터타입 수정

--- backend/src/mypage/serializers.py
+++ backend/src/mypage/serializers.py
@@ -286,7 +286,7 @@ class BookmarkSerializer(serializers.ModelSerializer):
     scholarship_name = serializers.CharField(source='scholarship.scholarship_name', read_only=True)
     start_date = serializers.DateField(source='scholarship.start_date', read_only=True)
     end_date = serializers.DateField(source='scholarship.end_date', read_only=True)
-    doc_id = serializers.IntegerField(source='scholarship.doc_id', read_only=True)
+    doc_id = serializers.CharField(source='scholarship.doc_id', read_only=True)
 
     class Meta:
         model = Bookmark
